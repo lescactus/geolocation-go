6abaa70b29e098593212223c113905160e19e26c
Alexandre Maldémé <alma@moodagent.com>
2022-06-21 16:02:39 +0200

    Add benchmarks for InMemory Get() and Save()

--- internal/repositories/geoip_inmemory_test.go
+++ internal/repositories/geoip_inmemory_test.go
@@ -159,3 +159,47 @@ func TestInMemoryDBStatus(t *testing.T) {
 		return
 	}
 }
+
+func BenchmarkInMemoryDBGet_EntryInMemoryDB(b *testing.B) {
+	m := NewInMemoryDB()
+	m.Save(context.Background(), &models.GeoIP{IP: "1.1.1.1"})
+
+	b.ResetTimer()
+	var ctx = context.Background()
+	for i := 0; i < b.N; i++ {
+		m.Get(ctx, "1.1.1.1")
+	}
+}
+
+func BenchmarkInMemoryDBGet_EntryNotInMemoryDB(b *testing.B) {
+	m := NewInMemoryDB()
+	m.Save(context.Background(), &models.GeoIP{IP: "1.1.1.1"})
+
+	b.ResetTimer()
+	var ctx = context.Background()
+	for i := 0; i < b.N; i++ {
+		m.Get(ctx, "2.2.2.2")
+	}
+}
+
+func BenchmarkInMemoryDBSave_EntryInMemoryDB(b *testing.B) {
+	m := NewInMemoryDB()
+	m.Save(context.Background(), &models.GeoIP{IP: "1.1.1.1"})
+
+	b.ResetTimer()
+	var ctx = context.Background()
+	for i := 0; i < b.N; i++ {
+		m.Save(ctx, &models.GeoIP{IP: "1.1.1.1"})
+	}
+}
+
+func BenchmarkInMemoryDBSave_EntryNotInMemoryDB(b *testing.B) {
+	m := NewInMemoryDB()
+	m.Save(context.Background(), &models.GeoIP{IP: "1.1.1.1"})
+
+	b.ResetTimer()
+	var ctx = context.Background()
+	for i := 0; i < b.N; i++ {
+		m.Save(ctx, &models.GeoIP{IP: "2.2.2.2"})
+	}
+}
